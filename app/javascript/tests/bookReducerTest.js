f79532963e8a5320d415a0bbdebbe83079db6bbf
Anda Popovici <andap.popovici@gmail.com>
2018-02-21 17:03:50 +0000

    Add Jest support

---
+++ app/javascript/tests/bookReducerTest.js
@@ -0,0 +1,7 @@
+const books = [{title: 'Title1', tags: ["Development", "Beginner"], displayed: true}]
+
+describe('book reducer', () => {
+  it('should show ', () => {
+    expect(true).toEqual(true)
+  }
+)})
