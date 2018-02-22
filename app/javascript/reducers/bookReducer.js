79602924405c3d566208342919ac426baa8edccb
Anda Popovici <andap.popovici@gmail.com>
2018-02-22 16:28:53 +0000

    Some unit tests

--- app/javascript/reducers/bookReducer.js
+++ app/javascript/reducers/bookReducer.js
@@ -1,8 +1,22 @@
 export default function bookReducer(state = [], action){
   switch (action.type){
     case 'TOGGLE_FILTER':
-      return state.map(book => book.tags.includes(action.filterName) ? 
-        {...book, displayed: action.enable} : book);
+    const result = state.map(book => {
+      if (book.tags.includes(action.filterName)) {
+        if (!action.enable) {
+          if (book.tags.length === 1){
+            return {...book, displayed: false}
+          } else {
+            return book;
+          }
+        } else {
+          return {...book, displayed: true}
+        }
+      } else {
+        return book;
+      }});
+      
+      return result;
     default:
       return state;
   }
--- app/javascript/tests/bookReducerTest.js
+++ app/javascript/tests/bookReducerTest.js
@@ -1,7 +1,63 @@
-const books = [{title: 'Title1', tags: ["Development", "Beginner"], displayed: true}]
+import bookReducer from '../reducers/bookReducer';
+
+const xBook = { title: 'X', tags: ["Dev", "Beginner"], displayed: true };
+const yBook = { title: 'Y', tags: ["Dev"], displayed: true };
+const zBook = { title: 'Z', tags: ["Agile", "Beginner"], displayed: true };
 
 describe('book reducer', () => {
-  it('should show ', () => {
-    expect(true).toEqual(true)
-  }
-)})
+  describe('TOGGLE_FILTER action', () => {
+    it('should keep books with tag displayed when filter enabled', () => {
+      const state = [xBook, yBook, zBook];
+  
+      expect(bookReducer(state, { type: 'TOGGLE_FILTER', filterName: "Dev", enable: true }))
+        .toEqual([{ title: 'X', tags: ["Dev", "Beginner"], displayed: true },
+                  { title: 'Y', tags: ["Dev"], displayed: true }, 
+                  { title: 'Z', tags: ["Agile", "Beginner"], displayed: true }])
+    })
+  
+    it('should hide books with tag when filter disabled', () => {
+      const state = [xBook, yBook, zBook];
+  
+      expect(bookReducer(state, { type: 'TOGGLE_FILTER', filterName: "Dev", enable: false }))
+        .toEqual([{ title: 'X', tags: ["Dev", "Beginner"], displayed: false },
+                  { title: 'Y', tags: ["Dev"], displayed: false }, 
+                  { title: 'Z', tags: ["Agile", "Beginner"], displayed: true }])
+    })
+  
+    it('should keep books with tag hidden when filter disabled', () => {
+      const state = [{...xBook, displayed: false}, yBook, {...zBook, displayed: false} ];
+  
+      expect(bookReducer(state, { type: 'TOGGLE_FILTER', filterName: "Beginner", enable: false }))
+        .toEqual([{ title: 'X', tags: ["Dev", "Beginner"], displayed: false },
+                  { title: 'Y', tags: ["Dev"], displayed: true }, 
+                  { title: 'Z', tags: ["Agile", "Beginner"], displayed: false }])
+    })
+  
+    it('should display books with tag when filter enabled ', () => {
+      const state = [{...xBook, displayed: false}, yBook, {...zBook, displayed: false} ];
+  
+      expect(bookReducer(state, { type: 'TOGGLE_FILTER', filterName: "Beginner", enable: true }))
+        .toEqual([{ title: 'X', tags: ["Dev", "Beginner"], displayed: true },
+                  { title: 'Y', tags: ["Dev"], displayed: true }, 
+                  { title: 'Z', tags: ["Agile", "Beginner"], displayed: true }])
+    })
+
+    it('disabling then enabling a filter returns same state ', () => {
+      const initialState = [xBook, yBook, zBook];
+      const disabledState = bookReducer(initialState, { type: 'TOGGLE_FILTER', filterName: "Dev", enable: false });
+      const finalState = bookReducer(disabledState, { type: 'TOGGLE_FILTER', filterName: "Agile", enable: true });
+
+      expect(finalState)
+        .toEqual(initialState);
+    })
+
+    it('combinations of filters works', () => {
+      const initialState = [xBook, yBook, zBook];
+      const disabledState = bookReducer(initialState, { type: 'TOGGLE_FILTER', filterName: "Dev", enable: false });
+      //const finalState = bookReducer(disabledState, { type: 'TOGGLE_FILTER', filterName: "Agile", enable: true });
+
+      expect(disabledState)
+        .toEqual(initialState);
+    })
+  })
+})
